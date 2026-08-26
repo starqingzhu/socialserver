51c10e93527cfb723361c807d6ee864c46a3143a
sunbin <starqingzhu@163.com>
2026-08-26 14:30:51 +0800

    开启热加载

--- internal/server.go
+++ internal/server.go
@@ -124,6 +124,9 @@ func (s *Server) OnInit() {
 	if err := configmgr.LoadConfigs(config.Default.ConfigDir); err != nil {
 		zaplog.LoggerSugar.Fatalf("OnInit: load configs failed: %v", err)
 	}
+	if err := configmgr.Global.StartWatch(); err != nil {
+		zaplog.LoggerSugar.Warnf("OnInit: start config watcher failed: %v", err)
+	}
 
 	if err := queue.InitQueues(); err != nil {
 		zaplog.LoggerSugar.Fatalf("OnInit: init queues failed: %v", err)
